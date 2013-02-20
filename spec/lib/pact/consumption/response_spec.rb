3d12f4a52507f82061a046440c52fbe19301c7cc
James Fraser <jfraser80@gmail.com>
2013-02-20 14:58:43 +1100

    Use 'expect' syntax in specs

--- spec/lib/pact/consumption/response_spec.rb
+++ spec/lib/pact/consumption/response_spec.rb
@@ -4,24 +4,30 @@ module Pact
   module Consumption
     describe Response do
 
-      subject { Response.new({ woot: /x/, britney: 'britney', nested: { foo: /bar/, baz: 'qux' }, array: ['first', /second/]} ) }
+      subject do
+        Response.new({
+          woot: /x/, britney: 'britney',
+          nested: { foo: /bar/, baz: 'qux' },
+          array: ['first', /second/]
+        })
+      end
 
       describe "reify" do
 
         it "converts regexes into real data" do
-          subject.reify[:woot].should == 'x'
+          expect(subject.reify[:woot]).to eql 'x'
         end
 
         it "passes strings through" do
-          subject.reify[:britney].should == 'britney'
+          expect(subject.reify[:britney]).to eql 'britney'
         end
 
         it "handles nested hashes" do
-          subject.reify[:nested].should == { foo: 'bar', baz: 'qux' }
+          expect(subject.reify[:nested]).to eql({ foo: 'bar', baz: 'qux' })
         end
 
         it "handles arrays" do
-          subject.reify[:array].should == ['first', 'second']
+          expect(subject.reify[:array]).to eql ['first', 'second']
         end
 
       end

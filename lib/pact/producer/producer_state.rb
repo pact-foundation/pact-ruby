f28498bcc20ef0adee063ef9838394b6ac7e167d
Duncan Bayne <duncan.bayne@rea-group.com>
2013-07-17 08:51:15 +1000

    Initial attempt at namespaces

--- lib/pact/producer/producer_state.rb
+++ lib/pact/producer/producer_state.rb
@@ -5,14 +5,21 @@ module Pact
       def producer_state name, &block
         Pact::Producer::ProducerState.producer_state(name, &block).register
       end
+
+      def producer_module name
+        ProducerState.current_namespaces << name
+        yield
+        ProducerState.current_namespaces.pop
+      end
     end
 
     class ProducerState
 
       attr_accessor :name
+      attr_accessor :namespace
 
       def self.producer_state name, &block
-        ProducerState.new(name, &block)
+        ProducerState.new(name, current_namespaces.join('.'), &block)
       end
 
       def self.register name, producer_state
@@ -23,16 +30,21 @@ module Pact
         @@producer_states ||= {}
       end
 
+      def self.current_namespaces
+        @@current_namespaces ||= []
+      end
+
       def self.get name
         producer_states[name] || producer_states[name.to_sym]
       end
 
       def register
-        self.class.register(name, self)
+        self.class.register(namespaced(name), self)
       end
 
-      def initialize name, &block
+      def initialize name, namespace, &block
         @name = name
+        @namespace = namespace
         instance_eval(&block)
       end
 
@@ -49,6 +61,16 @@ module Pact
           @tear_down_block = block
         elsif @tear_down_block
           instance_eval &@tear_down_block
+        end
+      end
+
+      private
+
+      def namespaced(name)
+        if namespace.empty?
+          name
+        else
+          "#{namespace}.#{name}"
         end
       end
     end
--- spec/lib/pact/producer/producer_state_spec.rb
+++ spec/lib/pact/producer/producer_state_spec.rb
@@ -3,7 +3,8 @@ require 'pact/producer/producer_state'
 
 module Pact
   module Producer
-    describe ProducerState do
+
+    describe 'global ProducerState' do
 
       MESSAGES = []
 
@@ -23,32 +24,96 @@ module Pact
         MESSAGES.clear
       end
 
-      subject { ProducerState.get("no_alligators") }
+      subject { ProducerState.get('no_alligators') }
 
-      describe "set_up" do
-        it "should call the block passed to set_up" do
+      describe 'set_up' do
+        it 'should call the block passed to set_up' do
           subject.set_up
           MESSAGES.should eq ['set_up']
         end
       end
 
-
-      describe "tear_down" do
-        it "should call the block passed to set_up" do
+      describe 'tear_down' do
+        it 'should call the block passed to set_up' do
           subject.tear_down
           MESSAGES.should eq ['tear_down']
         end
       end
 
-      describe ".get" do
-        context "when the name is a matching symbol" do
-          it "will return the ProducerState" do
-            ProducerState.get("no_alligators").should_not be_nil
+      describe '.get' do
+        context 'when the name is a matching symbol' do
+          it 'will return the ProducerState' do
+            ProducerState.get('no_alligators').should_not be_nil
           end
         end
-        context "when the name is a matching string" do
-          it "will return the ProducerState" do
-            ProducerState.get("some alligators").should_not be_nil
+        context 'when the name is a matching string' do
+          it 'will return the ProducerState' do
+            ProducerState.get('some alligators').should_not be_nil
+          end
+        end
+      end
+    end
+
+
+    describe 'namespaced ProducerStates' do
+
+      NAMESPACED_MESSAGES = []
+
+      producer_module :ns_one do
+        producer_state 'foo' do
+          set_up do
+            NAMESPACED_MESSAGES << 'ns_one.foo'
+          end
+        end
+      end
+
+      producer_module :ns_two do
+        producer_module :ns_three do
+          producer_state 'foo' do
+            set_up do
+              NAMESPACED_MESSAGES << 'ns_two.ns_three.foo'
+            end
+          end
+        end
+      end
+
+      before do
+        NAMESPACED_MESSAGES.clear
+      end
+
+      describe '.get' do
+
+        context 'the first namespace' do
+
+          it 'has a namespaced name' do
+            ProducerState.get('ns_one.foo').should_not be_nil
+          end
+        end
+
+        context 'the second namespace' do
+
+          it 'has a namespaced name' do
+            ProducerState.get('ns_two.ns_three.foo').should_not be_nil
+          end
+        end
+
+      end
+
+      describe 'set_up' do
+
+        context 'the first namespace' do
+
+          it 'runs its own setup' do
+            ProducerState.get('ns_one.foo').set_up
+            NAMESPACED_MESSAGES.should eq ['ns_one.foo']
+          end
+        end
+
+        context 'the second namespace' do
+
+          it 'runs its own setup' do
+            ProducerState.get('ns_two.ns_three.foo').set_up
+            NAMESPACED_MESSAGES.should eq ['ns_two.ns_three.foo']
           end
         end
       end

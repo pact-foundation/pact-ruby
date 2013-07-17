0dad1b81e77b480d84fb8f8afb7a4562a078e8ef
Beth <beth@bethesque.com>
2013-07-17 14:28:50 +1000

    Adding documentation for creating a pact:verify task

--- lib/pact/verification_task.rb
+++ lib/pact/verification_task.rb
@@ -1,6 +1,35 @@
 require 'rake/tasklib'
 require 'pact/producer/pact_spec_runner'
 
+=begin
+	To create a rake pact:verify:<something> task
+
+	Pact::VerificationTask.new(:head) do | pact |
+	  pact.uri 'http://master.cd.vpc.realestate.com.au/browse/BIQ-MAS/latestSuccessful/artifact/JOB2/Pacts/mas-contract_transaction_service.json',
+	              support_file: './spec/consumers/pact_helper', consumer: 'monitoring-and-alerting-system'
+    pact.uri 'http://master.cd.vpc.realestate.com.au/browse/BIQ-IMAGINARY-CONSUMER/latestSuccessful/artifact/JOB2/Pacts/imaginary_consumer-contract_transaction_service.json',
+                support_file: './spec/consumers/pact_helper', consumer: 'another-imaginary-consumer'
+	end
+
+	The pact.uri may be a local file system path or a remote URL.
+	The consumer option is used to specify the namespace for the producer states, useful if your producer has multiple consumers.
+	The support_file should include code that makes your rack app available for the rack testing framework.
+	Eg.
+
+	module PactTestApplication
+		def app
+		  TestApp.new
+		end
+	end
+
+	RSpec.configure do |config|
+		config.include PactTestApplication
+	end
+
+	It should also load all your app's dependencies (eg by calling out to spec_helper)
+
+=end
+
 module Pact
 	class VerificationTask < ::Rake::TaskLib
 	  attr_reader :pact_spec_config

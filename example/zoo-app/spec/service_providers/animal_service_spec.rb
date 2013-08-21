4acb77dde793d3775c5201b1eac787bcfb63037d
Beth <beth@bethesque.com>
2013-08-21 13:14:57 +1000

    Updating zoo-app spec

--- example/zoo-app/spec/service_providers/animal_service_spec.rb
+++ example/zoo-app/spec/service_providers/animal_service_spec.rb
@@ -2,14 +2,15 @@ require_relative 'pact_helper'
 
 module ZooApp
   describe "A pact with Animal Service", :pact => true do
+
+    before do
+      AnimalServiceClient.base_uri 'localhost:1234'
+    end
+
     describe "a call to get alligators" do
 
-      before do
-        AnimalServiceClient.base_uri 'localhost:1234'
-      end
-
       context "when valid response is received" do
-        it "returns a list of alligators" do
+        before do
           animal_service.
             given("there are alligators").
               upon_receiving("a request for alligators").
@@ -19,13 +20,16 @@ module ZooApp
                     headers: { 'Content-Type' => 'application/json' },
                     body: [{:name => 'Bob'}]
                   })
+        end
 
+        it "returns a list of alligators" do
           expect(AnimalServiceClient.find_alligators).to eq [ZooApp::Animals::Alligator.new(:name => 'Bob')]
         end
+
       end
 
-      context "when an non succesful response is returned" do
-        it "raises an error" do
+      context "when an error response is returned" do
+        before do
           animal_service.
             given("an error has occurred").
               upon_receiving("a request for alligators").
@@ -35,15 +39,19 @@ module ZooApp
                     headers: { 'Content-Type' => 'application/json' },
                     body: {:error => 'Argh!!!'}
                   })
-
-          expect{AnimalServiceClient.find_alligators}.to raise_error /Argh/
         end
+
+        it "raises an error" do
+          expect{ AnimalServiceClient.find_alligators }.to raise_error /Argh/
+        end
+
       end
     end
 
     describe "a call to get an alligator by name" do
       context "when an alligator by the given name exists" do
-        it "returns the alligator" do
+
+        before do
           animal_service.
             given("there is an alligator named Mary").
               upon_receiving("a request for alligator Mary").
@@ -53,13 +61,17 @@ module ZooApp
                     headers: { 'Content-Type' => 'application/json' },
                     body: {:name => 'Mary'}
                   })
+        end
 
+        it "returns the alligator" do
           expect(AnimalServiceClient.find_alligator_by_name("Mary")).to eq ZooApp::Animals::Alligator.new(:name => 'Mary')
         end
+
       end
 
       context "when an alligator by the given name does not exist" do
-        it "returns the alligator" do
+
+        before do
           animal_service.
             given("there is an alligator named Mary").
               upon_receiving("a request for alligator Mary").
@@ -68,9 +80,12 @@ module ZooApp
                     status: 404,
                     headers: { 'Content-Type' => 'application/json' }
                   })
+        end
 
+        it "returns nil" do
           expect(AnimalServiceClient.find_alligator_by_name("Mary")).to be_nil
         end
+
       end
     end
   end

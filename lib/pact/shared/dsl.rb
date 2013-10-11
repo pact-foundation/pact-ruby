3b11b42f9881a9b49eb23448ed49ed25ed7c329b
Beth <beth@bethesque.com>
2013-10-11 11:41:18 +1100

    Fixing the issue where a method defined in global scope could not be accessed in the DSL delegation code

--- lib/pact/shared/dsl.rb
+++ lib/pact/shared/dsl.rb
@@ -1,34 +1,56 @@
-require 'delegate'
-
 module Pact
 
-  class DslDelegator < SimpleDelegator
-    def instance_eval_with_previous_context_available(*args, &b)
-      meth = self.class.class_eval do
-        define_method :cloaker_, &b
-        meth = instance_method :cloaker_
-        remove_method :cloaker_
-        meth
-      end
-      with_previous_context(b.binding) {meth.bind(self).call(*args)}
+  class DslDelegator
+
+    def initialize delegation_target
+      @delegation_target = delegation_target
     end
 
-    def with_previous_context(binding, &block)
+    def instance_eval_with_previous_context_available(*args, &block)
+      with_previous_context_available(block.binding) do
+        bind_block_as_instance_method_on_self(&block).call(*args)
+      end
+    end
+
+    protected
+
+    def method_missing(method, *args, &block)
+      if delegation_target_responds_to? method
+        delegation_target.send(method, *args, &block)
+      else
+        previous_context.send(method, *args, &block)
+      end
+    end
+
+    private
+
+    attr_accessor :delegation_target, :previous_context
+
+    def bind_block_as_instance_method_on_self(&block)
+      create_instance_method_from_block(&block).bind(self)
+    end
+
+
+    def create_instance_method_from_block &block
+      meth = self.class.class_eval do
+        define_method :block_as_instance_method_, &block
+        meth = instance_method :block_as_instance_method_
+        remove_method :block_as_instance_method_
+        meth
+      end
+    end
+
+    def with_previous_context_available(binding, &block)
       @previous_context = binding.eval('self')
       result = block.call
       @previous_context = nil
       result
     end
 
-    def method_missing(method, *args, &block)
-      if __getobj__().respond_to? method
-        super
-      elsif @previous_context
-        @previous_context.send(method, *args, &block)
-      else
-        super
-      end
+    def delegation_target_responds_to?(method)
+      delegation_target.respond_to? method
     end
+
   end
 
 
@@ -37,17 +59,18 @@ module Pact
   # to access variables and methods defined in the calling scope.
   module DSL
     def build(*args, &block)
-      base = self.new(*args)
-      delegator_klass = self.const_get('DSL_DELEGATOR')
-      delegator = delegator_klass.new(base)
-      delegator.instance_eval_with_previous_context_available(&block)
-      base.finalize
-      base
+      new_instance_of_delegation_target_class = self.new(*args)
+      dsl_delegator_class = self.const_get('DSL_DELEGATOR_CLASS')
+      dsl_delegator = dsl_delegator_class.new(new_instance_of_delegation_target_class)
+      dsl_delegator.instance_eval_with_previous_context_available(&block)
+      new_instance_of_delegation_target_class.finalize
+      new_instance_of_delegation_target_class
     end
 
     def dsl(&block)
-      delegator_klass = Class.new(DslDelegator, &block)
-      self.const_set('DSL_DELEGATOR', delegator_klass)
+      dsl_delegator_class = Class.new(DslDelegator, &block)
+      self.const_set('DSL_DELEGATOR_CLASS', dsl_delegator_class)
     end
+
   end
 end
---
+++ spec/support/dsl_spec_support.rb
@@ -0,0 +1,7 @@
+def global_method
+  "I'm global"
+end
+
+def global_app
+  "I'm a global app"
+end

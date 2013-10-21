be4a466b184c529e0b62ea2a2a2589ba8c85b2ee
Stuart Liston <stuart.liston@gmail.com>
2013-10-21 12:40:07 +1100

    Altering JsonWarning so that it only warns once. Added spec to confirm that's the case.

--- lib/pact/json_warning.rb
+++ lib/pact/json_warning.rb
@@ -4,7 +4,8 @@ require 'json/add/regexp'
 module Pact
   module JsonWarning
     def check_for_active_support_json
-      @already_warned ||= false
+      return if @already_warned
+
       # Active support clobbers the as_json methods defined in the json/add directory of the json gem.
       # These methods are required to serialize and deserialize the Regexp and Symbol classes properly.
       # You can potentially fix this by making sure the json gem is required AFTER the active_support/json gem
@@ -16,12 +17,16 @@ module Pact
       # without breaking the calling code, which may depend on activesupport/json... then please fix this.
       # Note: we can probably do this in Ruby 2.0 with refinements, but for now, we're all stuck on 1.9 :(
 
-      unless @already_warned
-        unless Regexp.new('').as_json.is_a?(Hash)
-          Logger.new($stderr).warn("It appears you are using ActiveSupport json in your project. You are now in rubygems hell. Please see Pact::JsonWarning for more info.")
-          @already_warned = true
-        end
+      if as_json_clobbered?
+        Logger.new($stderr).warn("It appears you are using ActiveSupport json in your project. You are now in rubygems hell. Please see Pact::JsonWarning for more info.")
+        @already_warned = true
       end
+    end
+
+    private
+
+    def as_json_clobbered?
+      !Regexp.new('').as_json.is_a?(Hash)
     end
   end
 end

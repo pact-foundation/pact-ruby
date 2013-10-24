c94fc139ba4c2930e0c2f868df595c4aa0ebfa8b
Beth <beth@bethesque.com>
2013-10-24 19:26:44 +1100

    Updating example provider state

--- example/animal-service/spec/service_consumers/provider_states_for_zoo_app.rb
+++ example/animal-service/spec/service_consumers/provider_states_for_zoo_app.rb
@@ -1,9 +1,12 @@
 Pact.provider_states_for "Zoo App" do
   provider_state "there are alligators" do
-    #AlligatorRepo.save(Alligator.name("Mary"))
+    set_up do
+      #AlligatorRepo.save(Alligator.name("Mary"))
+    end
+
   end
 
   provider_state "there is not an alligator named Mary" do
-
+    no_op
   end
 end

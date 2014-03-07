82a5a329659a3d939fd6ac25de9a2131b10986b9
Beth <beth@bethesque.com>
2014-03-07 16:59:31 +1100

    Adding renderer

--- lib/pact/doc/markdown/interaction_renderer.rb
+++ lib/pact/doc/markdown/interaction_renderer.rb
@@ -1,9 +1,27 @@
+require 'pact/doc/markdown/interaction_view_model'
+
 module Pact
   module Doc
     module Markdown
-      def interaction interaction, pact
-        @interaction = interaction
-        @pact = pact
+      class InteractionRenderer
+
+        attr_reader :interaction
+
+        def initialize interaction, pact
+          @interaction = InteractionViewModel.new(interaction, pact)
+        end
+
+        def render
+          ERB.new(template_string).result(binding)
+        end
+
+        def template_string
+          File.read( template_file )
+        end
+
+        def template_file
+          File.dirname(__FILE__) + '/interaction.erb'
+        end
       end
     end
   end

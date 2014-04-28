78ca78cbc6d4bcec6df6cf77cfc94ea823c9b845
Beth <beth@bethesque.com>
2014-04-29 07:50:32 +1000

    Fixed bug where log_dir was being ignored when set to a non default value

--- lib/pact/configuration.rb
+++ lib/pact/configuration.rb
@@ -21,23 +21,28 @@ module Pact
     attr_accessor :log_dir
     attr_accessor :doc_dir
     attr_accessor :reports_dir
-    attr_accessor :logger
+    attr_writer :logger
     attr_accessor :tmp_dir
     attr_writer :pactfile_write_mode
 
     attr_accessor :error_stream
     attr_accessor :output_stream
 
-    def log_path
-      log_dir + "/pact.log"
+    def self.default_configuration
+      c = Configuration.new
+      c.pact_dir = File.expand_path('./spec/pacts')
+      c.tmp_dir = File.expand_path('./tmp/pacts')
+      c.log_dir = default_log_dir
+      c.pactfile_write_mode = :overwrite
+      c.reports_dir = File.expand_path('./reports/pacts')
+      c.doc_dir = File.expand_path("./doc")
+      c.output_stream = $stdout
+      c.error_stream = $stderr
+      c
     end
 
-    def pactfile_write_mode
-      if @pactfile_write_mode == :smart
-        is_rake_running? ? :overwrite : :update
-      else
-        @pactfile_write_mode
-      end
+    def logger
+      @logger ||= create_logger
     end
 
     def doc_generator= doc_generator
@@ -72,16 +77,39 @@ module Pact
       end
     end
 
+    def log_path
+      log_dir + "/pact.log"
+    end
+
+    def pactfile_write_mode
+      if @pactfile_write_mode == :smart
+        is_rake_running? ? :overwrite : :update
+      else
+        @pactfile_write_mode
+      end
+    end
+
     private
+
+    def self.default_log_dir
+      File.expand_path("./log")
+    end
 
     #Would love a better way of determining this! It sure won't work on windows.
     def is_rake_running?
       `ps -ef | grep rake | grep #{Process.ppid} | grep -v 'grep'`.size > 0
     end
+
+    def create_logger
+      FileUtils::mkdir_p log_dir
+      logger = Logger.new(log_path)
+      logger.level = Logger::DEBUG
+      logger
+    end
   end
 
   def self.configuration
-    @configuration ||= default_configuration
+    @configuration ||= Configuration.default_configuration
   end
 
   def self.configure
@@ -90,34 +118,7 @@ module Pact
   end
 
   def self.clear_configuration
-    @configuration = default_configuration
-  end
-
-  private
-
-  def self.default_configuration
-    c = Configuration.new
-    c.pact_dir = File.expand_path('./spec/pacts')
-    c.tmp_dir = File.expand_path('./tmp/pacts')
-    c.log_dir = default_log_dir
-    c.logger = default_logger c.log_path
-    c.pactfile_write_mode = :overwrite
-    c.reports_dir = File.expand_path('./reports/pacts')
-    c.doc_dir = File.expand_path("./doc")
-    c.output_stream = $stdout
-    c.error_stream = $stderr
-    c
-  end
-
-  def self.default_log_dir
-    File.expand_path("./log")
-  end
-
-  def self.default_logger path
-    FileUtils::mkdir_p File.dirname(path)
-    logger = Logger.new(path)
-    logger.level = Logger::DEBUG
-    logger
+    @configuration = nil
   end
 
 end
--- spec/lib/pact/configuration_spec.rb
+++ spec/lib/pact/configuration_spec.rb
@@ -25,6 +25,26 @@ describe Pact do
   describe Pact::Configuration do
     let(:configuration) { Pact::Configuration.new }
 
+    describe "log_dir" do
+      it "sets the location of the logs" do
+        expect(Logger).to receive(:new).with("./tmp/logs/pact.log").and_call_original
+        Pact.configure do | config |
+          config.log_dir = "./tmp/logs"
+        end
+        Pact.configuration.logger
+      end
+    end
+
+    describe "logger" do
+      it "sets the location of the logs to log_dir by default" do
+        expect(Logger).to receive(:new).with(File.expand_path("./log/pact.log")).and_call_original
+        Pact.configuration.logger
+      end
+      it "defaults to DEBUG" do
+        expect(Pact.configuration.logger.level).to eq Logger::DEBUG
+      end
+    end
+
     describe "doc_generator" do
 
       context "with a symbol" do

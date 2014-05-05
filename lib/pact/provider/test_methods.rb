dc9855b9228deaf775e85db4c98a22ec0b175d08
Beth <beth@bethesque.com>
2014-05-05 12:29:51 +1000

    Downcasing HTTP methods before sending call to RSpec::Test::Methods because pact-jvm is using an upcase method https://github.com/DiUS/pact-jvm/issues/34

--- lib/pact/provider/test_methods.rb
+++ lib/pact/provider/test_methods.rb
@@ -18,9 +18,9 @@ module Pact
         request = Request::Replayable.new(interaction.request)
         args = [request.path, request.body, request.headers]
 
-        logger.info "Sending #{request.method} request to path: \"#{request.path}\" with headers: #{request.headers}, see debug logs for body"
+        logger.info "Sending #{request.method.upcase} request to path: \"#{request.path}\" with headers: #{request.headers}, see debug logs for body"
         logger.debug "body :#{request.body}"
-        response = self.send(request.method, *args)
+        response = self.send(request.method.downcase, *args)
         logger.info "Received response with status: #{response.status}, headers: #{response.headers}, see debug logs for body"
         logger.debug "body: #{response.body}"
       end

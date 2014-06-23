68614df59970f1b9851e89679dd648f332ea013b
Beth <beth@bethesque.com>
2014-06-23 20:24:02 +1000

    Writing tests and thoughts about text diffing

--- spec/lib/pact/shared/text_differ_spec.rb
+++ spec/lib/pact/shared/text_differ_spec.rb
@@ -4,13 +4,51 @@ require 'pact/shared/text_differ'
 module Pact
   describe TextDiffer do
 
-    extend Matchers
-
     describe ".call" do
 
-      it "fails when the actual object is not a string"
+      subject { TextDiffer.call expected, actual }
+
+      let(:expected) { "This is the string you are looking for" }
+
+      context "when the expected and actual are both strings" do
+
+        context "when they equal each other" do
+          let(:actual) { "This is the string you are looking for" }
+
+          it "returns an empty diff" do
+            expect(subject.any?).to be false
+          end
+        end
+
+        context "when they don't equal each other" do
+          let(:actual) { "This is not the string you are looking for" }
+
+          it "returns the diff" do
+            expect(subject).to eq Pact::Matchers::Difference.new(expected, actual)
+          end
+        end
+      end
+
+      context "when the actual is not a String" do
+        let(:actual) { {some: 'hash'} }
+        let(:difference) { Pact::Matchers::Difference.new(expected, actual)}
+        it "returns the diff" do
+          expect(subject)
+        end
+      end
+
+      context "when the expected and actual are both objects" do
+        let(:actual) { {some: 'hash', blah: 'blah'} }
+        let(:expected) { {some: 'hash'} }
+        let(:difference) { Pact::Matchers::Difference.new(expected, actual)}
+
+        it "returns the diff using the JSON matching logic, allowing extra keys. But should it really if the expected Content-Type isn't actually JSON?" do
+          expect(subject)
+        end
+
+        it "potentially should treat both expected and actual as Strings"
+      end
 
     end
-
   end
 end

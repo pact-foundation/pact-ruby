ca883a8b17fe50f9b53ada6cff291bec801490ee
Beth <beth@bethesque.com>
2014-08-18 15:53:47 +1000

    Made options optional for ConsumerContactBuilder.wait_for_interactions

--- lib/pact/consumer/consumer_contract_builder.rb
+++ lib/pact/consumer/consumer_contract_builder.rb
@@ -55,7 +55,7 @@ module Pact
         mock_service_client.write_pact @consumer_contract_details
       end
 
-      def wait_for_interactions options
+      def wait_for_interactions options = {}
         wait_max_seconds = options.fetch(:wait_max_seconds, 3)
         poll_interval = options.fetch(:poll_interval, 0.1)
         mock_service_client.wait_for_interactions wait_max_seconds, poll_interval
@@ -70,54 +70,6 @@ module Pact
 
       attr_reader :mock_service_client
       attr_writer :interaction_builder
-
-      # def interactions_for_new_consumer_contract pactfile_write_mode
-      #   pactfile_write_mode == :update ? existing_interactions : []
-      # end
-
-      # def filter interactions, pactfile_write_mode
-      #   if pactfile_write_mode == :update
-      #     UpdatableInteractionsFilter.new(interactions)
-      #   else
-      #     DistinctInteractionsFilter.new(interactions)
-      #   end
-      # end
-
-      # def warn_and_stderr msg
-      #   Pact.configuration.error_stream.puts msg
-      #   logger.warn msg
-      # end
-
-      # def info_and_puts msg
-      #   $stdout.puts msg
-      #   logger.info msg
-      # end
-
-      # def existing_interactions
-      #   interactions = []
-      #   if pactfile_exists?
-      #     begin
-      #       interactions = existing_consumer_contract.interactions
-      #       info_and_puts "*****************************************************************************"
-      #       info_and_puts "Updating existing file .#{consumer_contract.pactfile_path.gsub(Dir.pwd, '')} as config.pactfile_write_mode is :update"
-      #       info_and_puts "Only interactions defined in this test run will be updated."
-      #       info_and_puts "As interactions are identified by description and provider state, pleased note that if either of these have changed, the old interactions won't be removed from the pact file until the specs are next run with :pactfile_write_mode => :overwrite."
-      #       info_and_puts "*****************************************************************************"
-      #     rescue StandardError => e
-      #       warn_and_stderr "Could not load existing consumer contract from #{consumer_contract.pactfile_path} due to #{e}"
-      #       warn_and_stderr "Creating a new file."
-      #     end
-      #   end
-      #   interactions
-      # end
-
-      # def pactfile_exists?
-      #   File.exist?(consumer_contract.pactfile_path)
-      # end
-
-      # def existing_consumer_contract
-      #   Pact::ConsumerContract.from_uri(consumer_contract.pactfile_path)
-      # end
 
     end
   end

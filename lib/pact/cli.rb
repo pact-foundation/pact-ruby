df3342fd88dbcb74beca050836f18cef1c280b75
Beth <beth@bethesque.com>
2014-10-12 12:39:56 +1100

    Removing pact server command as it is now in pact-mock_service

--- lib/pact/cli.rb
+++ lib/pact/cli.rb
@@ -15,15 +15,6 @@ module Pact
       RunPactVerification.call(options)
     end
 
-    desc 'service', "Start a mock service"
-    method_option :port, aliases: "-p", desc: "Port on which to run the service"
-    method_option :log, aliases: "-l", desc: "File to which to log output"
-    method_option :quiet, aliases: "-q", desc: "If true, no admin messages will be shown"
-
-    def service
-      RunStandaloneMockService.call(options)
-    end
-
     private
 
     def log message
@@ -96,23 +87,6 @@ module Pact
       {criteria: SpecCriteria.call}
     end
 
-  end
-
-  class RunStandaloneMockService
-
-    def self.call options
-      service_options = {}
-      if options[:log]
-        log = File.open(options[:log], 'w')
-        log.sync = true
-        service_options[:log_file] = log
-      end
-
-      port = options[:port] || FindAPort.available_port
-      mock_service = Consumer::MockService.new(service_options)
-      trap(:INT) { Rack::Handler::WEBrick.shutdown }
-      Rack::Handler::WEBrick.run(mock_service, :Port => port, :AccessLog => [])
-    end
   end
 
   class SpecCriteria

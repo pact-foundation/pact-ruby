c64e122d8e69028737c494c2ded11f6d25cba724
Michael Treacher <Michael.Treacher@rea-group.com>
2014-10-23 21:22:13 +1100

    Add the ability to use regex in headers when defining the response.

--- spec/integration/consumer_spec.rb
+++ spec/integration/consumer_spec.rb
@@ -113,6 +113,7 @@ describe "A service consumer side of a pact", :pact => true  do
     end
 
     let(:body) { 'That is some good Mallory.' }
+    let(:zebra_header) { '*.zebra.com' }
 
     it "goes like this" do
       zebra_service.
@@ -124,12 +125,16 @@ describe "A service consumer side of a pact", :pact => true  do
         }).
         will_respond_with({
           status: 200,
-          headers: { 'Content-Type' => 'text/html' },
+          headers: { 
+            'Content-Type' => 'text/html',
+            'Zebra-Origin' => Pact::Term.new(matcher: /\*/, generate: zebra_header)
+          },
           body: Pact::Term.new(matcher: /Mallory/, generate: body)
         })
 
       response = Faraday.get(zebra_service.mock_service_base_url + "/mallory", nil, {'Accept' => 'text/html'})
       expect(response.body).to eq body
+      expect(response.headers['Zebra-Origin']).to eq zebra_header
 
       interactions = Pact::ConsumerContract.from_json(zebra_service.write_pact).interactions
       expect(interactions.first.provider_state).to eq("the_zebras_are_here")

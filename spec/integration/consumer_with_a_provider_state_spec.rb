0034254fd6bc35ddeb47b8cba4d3238878fb6bb9
Beth <beth@bethesque.com>
2014-10-24 16:20:05 +1100

    Adding integration test for using Pact::Term with headers

--- spec/integration/consumer_with_a_provider_state_spec.rb
+++ spec/integration/consumer_with_a_provider_state_spec.rb
@@ -19,23 +19,28 @@ describe "A service consumer side of a pact", :pact => true  do
     end
 
     let(:body) { 'That is some good Mallory.' }
+    let(:zebra_header) { '*.zebra.com' }
 
     it "goes like this" do
       zebra_service.
         given(:the_zebras_are_here).
-      upon_receiving("a retrieve Mallory request").with({
-                                                          method: :get,
-                                                          path: '/mallory',
-                                                          headers: {'Accept' => 'text/html'}
-      }).
-      will_respond_with({
-                          status: 200,
-                          headers: { 'Content-Type' => 'text/html' },
-                          body: Pact::Term.new(matcher: /Mallory/, generate: body)
-      })
+      upon_receiving("a retrieve Mallory request").with(
+        method: :get,
+        path: '/mallory',
+        headers: {'Accept' => 'text/html'}
+      ).
+      will_respond_with(
+        status: 200,
+        headers: {
+          'Content-Type' => 'text/html',
+          'Zebra-Origin' => Pact::Term.new(matcher: /\*/, generate: zebra_header)
+        },
+        body: Pact::Term.new(matcher: /Mallory/, generate: body)
+      )
 
       response = Faraday.get(zebra_service.mock_service_base_url + "/mallory", nil, {'Accept' => 'text/html'})
       expect(response.body).to eq body
+      expect(response.headers['Zebra-Origin']).to eq zebra_header
 
       interactions = Pact::ConsumerContract.from_json(zebra_service.write_pact).interactions
       expect(interactions.first.provider_state).to eq("the_zebras_are_here")

c1881c4bafba833ccb75a8ba48b1b81254cc27df
Beth <beth@bethesque.com>
2015-07-09 14:01:13 +1000

    Added test to show mock service will use v2 matching rules when the pact specification version is set to 2

---
+++ spec/integration/consumer_with_v2_matching.rb
@@ -0,0 +1,47 @@
+require 'pact/consumer'
+require 'pact/consumer/rspec'
+load 'pact/consumer/world.rb'
+require 'faraday'
+
+describe "When the pact_specification_version is set to 2", :pact => true  do
+
+  before do
+    Pact.clear_configuration
+
+    Pact.service_consumer "Consumer" do
+      has_pact_with "Zebra Service" do
+        mock_service :zebra_service do
+          verify false
+          port 1235
+          pact_specification_version '2'
+        end
+      end
+    end
+  end
+
+  let(:body) { 'That is some good Mallory.' }
+  let(:zebra_header) { '*.zebra.com' }
+
+  it "writes the pact with v2 matching rules" do
+    zebra_service.
+      given(:the_zebras_are_here).
+    upon_receiving("a retrieve Mallory request").with(
+      method: :get,
+      path: '/mallory',
+      headers: {'Accept' => 'text/html'}
+    ).
+    will_respond_with(
+      status: 200,
+      headers: {
+        'Content-Type' => 'text/html'
+      },
+      body: Pact::Term.new(matcher: /Mallory/, generate: body)
+    )
+
+    response = Faraday.get(zebra_service.mock_service_base_url + "/mallory", nil, {'Accept' => 'text/html'})
+    expect(response.body).to eq body
+    pact_hash = JSON.parse(zebra_service.write_pact)
+    expect(pact_hash['interactions'][0]['response']['matchingRules']).to be_instance_of(Hash)
+  end
+
+end

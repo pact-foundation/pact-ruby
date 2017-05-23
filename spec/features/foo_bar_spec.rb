c54a3a91ce3340b8bfa73d766245553c6c1c714f
Beth Skurrie <beth@bethesque.com>
2017-05-23 15:45:41 +1000

    Add specs and tasks for doing manual end to end testing (create, publish, verify, publish verification)

---
+++ spec/features/foo_bar_spec.rb
@@ -0,0 +1,43 @@
+require 'net/http'
+require 'pact/consumer'
+require 'pact/consumer/rspec'
+load 'pact/consumer/world.rb'
+
+# Use this test along with `rake pact:verify:foobar` to debug end to end issues.
+# The Bar app is in spec/support/bar_pact_helper.rb
+
+describe "bethtest" do
+  it "foo", :pact => true  do
+
+      Pact.clear_configuration
+      Pact.clear_consumer_world
+
+      Pact.service_consumer "Foo" do
+        has_pact_with "Bar" do
+          mock_service :bar_service do
+            pact_specification_version "2"
+            port 4638
+          end
+        end
+      end
+
+      bar_service.
+        upon_receiving("a retrieve thing request").with({
+        method: :get,
+        path: '/thing'
+      }).
+        will_respond_with({
+        status: 200,
+        headers: { 'Content-Type' => 'application/json' },
+        body: Pact.each_like({status: Pact.term(/\d+/, "4")})
+      })
+
+      bar_response = Net::HTTP.get_response(URI('http://localhost:4638/thing'))
+
+      expect(bar_response.code).to eql '200'
+      expect(JSON.parse(bar_response.body)).to eq [{"status" => "4"}]
+
+      puts bar_service.write_pact
+  end
+
+end
---
+++ spec/support/bar_pact_helper.rb
@@ -0,0 +1,26 @@
+require 'json'
+require 'pact/provider/rspec'
+
+module Pact
+	module Test
+		class BarApp
+			def call env
+				[200, {'Content-Type' => 'application/json'}, [{"status" => "5"},{"status" => "6"}].to_json]
+			end
+		end
+
+		Pact.configure do | config |
+			config.logger.level = Logger::DEBUG
+		end
+
+		Pact.service_provider "Bar" do
+			app { BarApp.new }
+			app_version '1.2.3'
+			publish_verification_results true
+
+			honours_pact_with 'Foo' do
+				pact_uri './spec/support/foo-bar.json'
+			end
+		end
+	end
+end

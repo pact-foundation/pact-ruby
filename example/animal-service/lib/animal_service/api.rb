e1e6f4c56b7837a90f9530176c8a91fd2ebe8c30
Beth Skurrie <beth@bethesque.com>
2017-12-22 08:30:13 +1100

    chore: update examples

--- example/animal-service/lib/animal_service/api.rb
+++ example/animal-service/lib/animal_service/api.rb
@@ -11,14 +11,14 @@ module AnimalService
 
     error do
       e = env['sinatra.error']
-      content_type :json
+      content_type :json, :charset => 'utf-8'
       status 500
       {error: e.message, backtrace: e.backtrace}.to_json
     end
 
     get '/alligators/:name' do
       if (alligator = AnimalRepository.find_alligator_by_name(params[:name]))
-        content_type :json
+        content_type :json, :charset => 'utf-8'
         alligator.to_json
       else
         status 404

69552f80bfc043675260e9590e40383e83109213
Beth Skurrie <beth@bethesque.com>
2018-05-15 07:55:29 +1000

    style: rename pact_entity to index_entity
    Make it clearer what this entity actually is.

--- lib/pact/pact_broker/fetch_pacts.rb
+++ lib/pact/pact_broker/fetch_pacts.rb
@@ -4,7 +4,7 @@ require 'pact/hal/http_client'
 module Pact
   module PactBroker
     class FetchPacts
-      attr_reader :provider, :tags, :broker_base_url, :basic_auth_options, :http_client, :pact_entity
+      attr_reader :provider, :tags, :broker_base_url, :basic_auth_options, :http_client, :index_entity
 
       LATEST_PROVIDER_TAG_RELATION = 'pb:latest-provider-pacts-with-tag'.freeze
       LATEST_PROVIDER_RELATION = 'pb:latest-provider-pacts'.freeze
@@ -35,18 +35,18 @@ module Pact
 
       def get_index
         response = http_client.get(broker_base_url)
-        @pact_entity = Pact::Hal::Entity.new(response.body, http_client)
+        @index_entity = Pact::Hal::Entity.new(response.body, http_client)
       end
 
       def get_latest_tagged_pacts_for_provider
-        link = pact_entity._link(LATEST_PROVIDER_TAG_RELATION)
+        link = index_entity._link(LATEST_PROVIDER_TAG_RELATION)
         tags.collect do | tag |
           get_pact_urls(link.expand(provider: provider, tag: tag).get)
         end.flatten
       end
 
       def get_latest_pacts_for_provider
-        link = pact_entity._link(LATEST_PROVIDER_RELATION)
+        link = index_entity._link(LATEST_PROVIDER_RELATION)
         get_pact_urls(link.expand(provider: provider).get)
       end
 

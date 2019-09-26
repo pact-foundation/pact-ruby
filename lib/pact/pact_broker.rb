9ffbaa584738afc029fd0bb2c93074685e4a4d75
Beth Skurrie <beth@bethesque.com>
2019-09-26 16:29:48 +1000

    feat: put back changed method so this is not a breaking change

--- lib/pact/pact_broker.rb
+++ lib/pact/pact_broker.rb
@@ -9,6 +9,11 @@ module Pact
   module PactBroker
     extend self
 
+    # Keep for backwards compatibility with pact-provider-verifier < 1.23.1
+    def fetch_pact_uris *args
+      Pact::PactBroker::FetchPacts.call(*args).collect(&:uri)
+    end
+
     def fetch_pact_uris_for_verification *args
       Pact::PactBroker::FetchPactURIsForVerification.call(*args)
     end

ece6611d696ed8bc2e6303cfc7b6a217b3b5e7a8
Beth Skurrie <beth@bethesque.com>
2020-01-22 12:45:34 +1100

    chore: remove gemfiles dir from release script

--- script/release.sh
+++ script/release.sh
@@ -3,7 +3,7 @@ set -e
 unset X_PACT_DEVELOPMENT
 bundle exec bump ${1:-minor} --no-commit
 bundle exec rake generate_changelog
-git add CHANGELOG.md lib/pact/version.rb gemfiles
+git add CHANGELOG.md lib/pact/version.rb
 git commit -m "chore(release): version $(ruby -r ./lib/pact/version.rb -e "puts Pact::VERSION")" && git push
 bundle exec rake tag_for_release
 

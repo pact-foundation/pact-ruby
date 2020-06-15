fe3ba6fe6e98db74497c9f185cdc15abd07684aa
Max Forasteiro <max.forasteiro@gmail.com>
2020-06-15 11:35:27 -0300

    Fix integration with pact-message-ruby

--- lib/pact/provider/configuration/message_provider_dsl.rb
+++ lib/pact/provider/configuration/message_provider_dsl.rb
@@ -22,6 +22,33 @@ module Pact
         end
 
         dsl do
+          def app &block
+            self.app_block = block
+          end
+
+          def app_version application_version
+            self.application_version = application_version
+          end
+
+          def app_version_tags tags
+            self.tags = tags
+          end
+
+          def publish_verification_results publish_verification_results
+            self.publish_verification_results = publish_verification_results
+            Pact::RSpec.with_rspec_2 do
+              Pact.configuration.error_stream.puts "WARN: Publishing of verification results is currently not supported with rspec 2. If you would like this functionality, please feel free to submit a PR!"
+            end
+          end
+
+          def honours_pact_with consumer_name, options = {}, &block
+            create_pact_verification consumer_name, options, &block
+          end
+
+          def honours_pacts_from_pact_broker &block
+            create_pact_verification_from_broker &block
+          end
+
           def builder &block
             self.app_block = lambda { RackToMessageAdapter.new(block)  }
           end

c639ecba72bc9194f44a5001dea788e8422eeb90
Beth Skurrie <beth.skurrie@smartbear.com>
2023-10-23 09:36:50 +1100

    chore: support rack 2 and rack 3

--- lib/pact/provider/configuration/service_provider_dsl.rb
+++ lib/pact/provider/configuration/service_provider_dsl.rb
@@ -21,7 +21,13 @@ module Pact
           unless File.exist? Pact.configuration.config_ru_path
             raise "Could not find config.ru file at #{Pact.configuration.config_ru_path} Please configure the service provider app or create a config.ru file in the root directory of the project. See https://github.com/pact-foundation/pact-ruby/wiki/Verifying-pacts for more information."
           end
-          Rack::Builder.parse_file(Pact.configuration.config_ru_path)
+          result = Rack::Builder.parse_file(Pact.configuration.config_ru_path)
+
+          if result.respond_to?(:first) # rack 2
+            result.first
+          else # rack 3
+            result
+          end
         }
 
         def initialize name

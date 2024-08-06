9e03e10e7fe50bd0c238aec2a242aeefe5b4c5d2
Yousaf Nabi <yousaf@pactflow.io>
2024-08-06 22:52:36 +0100

    chore: add example of fail_if_no_pacts_found from pact broker

--- example/animal-service/spec/service_consumers/pact_helper.rb
+++ example/animal-service/spec/service_consumers/pact_helper.rb
@@ -8,4 +8,11 @@ Pact.service_provider 'Animal Service' do
     pact_uri '../zoo-app/spec/pacts/zoo_app-animal_service.json'
   end
 
+  ## For pact contracts from a Pact Broker
+
+  # honours_pacts_from_pact_broker do
+  #   pact_broker_base_url 'http://localhost:9292'
+  #   # fail_if_no_pacts_found false # defaults to true
+  # end
+
 end

5bd246136fdaca5342160b1a1cf07c0d8494d2f7
Yousaf Nabi <you@saf.dev>
2025-10-11 01:20:34 +0100

    chore: add demo of message example without kafka

---
+++ spec/internal/app/producers/test_message_producer.rb
@@ -0,0 +1,12 @@
+class TestMessageProducer
+
+def publish_message()
+  message = {
+    "email": "jane@example.com",
+    "title": "Miss",
+    "first_name": "Jane",
+    "surname": "Doe"   
+  }
+end
+
+end

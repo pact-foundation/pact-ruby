246abc102627eb6836697f48885b082b2115f064
Yousaf Nabi <you@saf.dev>
2025-10-22 11:03:15 +0100

    chore: add load/rescue block for pact/v2 namespace

--- lib/pact.rb
+++ lib/pact.rb
@@ -4,4 +4,9 @@ require 'pact/configuration'
 require 'pact/consumer'
 require 'pact/provider'
 require 'pact/consumer_contract'
-require 'pact/v2'
+
+begin
+  require 'pact/v2'
+rescue LoadError => e
+  warn "Warning: Could not load 'pact/v2': #{e.message} \nPlease ensure that the 'pact-ffi' gem is included in your Gemfile for pact/v2 support."
+end

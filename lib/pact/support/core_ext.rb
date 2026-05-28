74bc9a6c47d0eb3feb990366495022f691e308b5
Yousaf Nabi <yousaf@pactflow.io>
2026-05-28 19:10:41 +0100

    chore: add presence method to Object polyfill (for non activesupport)

--- lib/pact/support/core_ext.rb
+++ lib/pact/support/core_ext.rb
@@ -38,6 +38,10 @@ unless Object.method_defined?(:blank?)
   class Object
     def blank? = respond_to?(:empty?) ? !!empty? : !self
     def present? = !blank?
+
+    def presence
+      self if present?
+    end
   end
 end
 

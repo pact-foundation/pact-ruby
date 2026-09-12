51300904d5bf8805262373ba40a00d8b7c36da5f
Yousaf Nabi <you@saf.dev>
2026-09-12 03:43:41 +0100

    chore: fixup example json arg

--- example/zoo-app/lib/zoo_app/animal_service_client.rb
+++ example/zoo-app/lib/zoo_app/animal_service_client.rb
@@ -24,7 +24,7 @@ module ZooApp
     end
 
     def self.parse_body response
-      JSON.parse(response.body, { :symbolize_names => true })
+      JSON.parse(response.body, :symbolize_names => true)
     end
   end
 end
